fa259e9665de057ca5c9376dce9f57769bb1aa62
geraa1985 <geraa1985@gmail.com>
2021-02-12 21:27:07 +0300

    Initial commit / add dependencies

---
+++ app/src/main/java/com/geraa1985/phrasebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geraa1985.phrasebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
