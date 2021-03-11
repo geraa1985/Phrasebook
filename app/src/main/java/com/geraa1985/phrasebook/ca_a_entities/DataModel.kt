f3be9b693f56fea711df0b953ca0ec589c17c201
geraa1985 <geraa1985@gmail.com>
2021-03-11 10:30:08 +0300

    Add some changes

--- app/src/main/java/com/geraa1985/phrasebook/ca_a_entities/DataModel.kt
+++ app/src/main/java/com/geraa1985/phrasebook/ca_a_entities/DataModel.kt
@@ -3,7 +3,7 @@ package com.geraa1985.phrasebook.ca_a_entities
 import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class DataModel(
